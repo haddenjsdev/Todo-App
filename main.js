6f534aaab281d60d5936b3e12d8f03bb2e66e7e1
bxh8998 <bryan_hadden@homedepot.com>
2018-05-17 00:55:45 -0400

    added javascript

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
- var todoWrapper;
+var SearchFilter = function (id) {
+    var id = id;
+    var domNode = document.getElementById(id);
+    var callback = null;
+    var button = document.querySelector('.input2');
+    var listWrapper = document.querySelector('.list-wrapper');
+    button.addEventListener('click', function() {
+      addItem(listWrapper);
+    });
+}
 
- todoWrapper.document.querySelector('.todo-wrapper');
+SearchFilter.prototype.init = function () {
+}
+
+function addItem(listWrapper) {
+    var input = document.querySelector('.input1');
+    var newItem = document.createElement("div");
+    var itemTitle = document.createElement("p");
+    var metaWrapper = document.createElement("div");
+    var span = document.createElement("span");
+
+    newItem.setAttribute('class', 'todo-item');
+    itemTitle.setAttribute('class', 'todo-item-title');
+    metaWrapper.setAttribute('class', 'meta-wrapper');
+    itemTitle.innerHTML = '';
+    itemTitle.innerHTML = input.value;
+    span.innerHTML = 'May 15, 8pm ';
+
+    if(input.value != ''){
+      listWrapper.appendChild(newItem);
+      newItem.appendChild(itemTitle);
+      newItem.appendChild(metaWrapper);
+      metaWrapper.appendChild(span);
+      var removeError = document.querySelector('.errorDiv');
+      if(removeError){
+        listWrapper.removeChild(removeError);
+      }
+      console.dir(input.value);
+    }else{
+      if(errorDiv != true){
+        var errorDiv = document.createElement("div");
+        var errorText = document.createElement("p");
+        errorDiv.setAttribute('class', 'errorDiv');
+        errorText.setAttribute('class', 'error');
+        errorText.innerHTML = 'ERROR! Please enter something....';
+        errorDiv.appendChild(errorText);
+        listWrapper.appendChild(errorDiv);
+      }
+    }
+
+}
